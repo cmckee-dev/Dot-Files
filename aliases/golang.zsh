3c96961c75b1ff82881b3c6460544047a82828be
Curtis Mckee <cmckee.dev@gmail.com>
2019-01-04 22:26:25 -0700

    Updating path typo

--- aliases/golang.zsh
+++ aliases/golang.zsh
@@ -1,4 +1,4 @@
 alias gop='cd $GOPATH'
 alias gopb='cd $GOPATH/bin'
 alias gops='cd $GOPATH/src'
-alias gopcmd='cd $GOPATH/src/github/cmckee-dev'
+alias gopcmd='cd $GOPATH/src/github.com/cmckee-dev'
